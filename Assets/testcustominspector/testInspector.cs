7a14a6c6c6ae788e1a04b3de109409a31f09a90a
ben <benjaminshurts@gmail.com>
2021-09-25 10:36:58 -0600

    custom inspector fixed

--- Assets/testcustominspector/testInspector.cs
+++ Assets/testcustominspector/testInspector.cs
@@ -5,6 +5,7 @@ using UnityEditor;
 using UnityEditor.Rendering;
 using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
 [CustomEditor(typeof(testwork))]
@@ -30,29 +31,34 @@ public class testInspector : Editor
 
     public override VisualElement CreateInspectorGUI()
     {
-        var root = _root;
-        root.Clear();
+        _root = _visualTree.Instantiate();
 
-        _visualTree.CloneTree(_root);
+        var testval2 = _root.Q<IntegerField>("test2");
+        var testval = _root.Q<FloatField>("test1");
+        
+        //var testval2 = _root.Q<DebugUI.IntField>("test2");
+        
+        /*
+        var testval2 = _root.Q<FloatField>("test2");
+        var progbar = _root.Q<ProgressBar>("progress");
+        */
 
-        var testval = root.Q<FloatField>("test1");
-        var testval2 = root.Q<FloatField>("test2");
-        var progbar = root.Q<ProgressBar>("progress");
-
-        progbar.value = _testwork.TestFloat1 / _testwork.TestFloat2;
+        //progbar.value = _testwork.TestFloat1 / _testwork.TestFloat2;
         
         //SerializedProperty objectName = _testwork.
         //SerializedObject so = new SerializedObject()
-        GameObject selectedObj = Selection.activeObject as GameObject;
+        //GameObject selectedObj = Selection.activeObject as GameObject;
         
-        SerializedObject so = new SerializedObject(selectedObj);
-        _root.Bind(so);
+        //SerializedObject so = new SerializedObject(selectedObj);
+        //_root.Bind(so);
         
-        /*
+        
+        
         testval.BindProperty(serializedObject.FindProperty("testFloat1"));
         testval2.BindProperty(serializedObject.FindProperty("testFloat2"));
-        */
 
+        serializedObject.ApplyModifiedProperties();
+        
         return _root;
     }
 }
