2bf66d9fa13d7da6d9639ca8adf4b5051c33c05b
ben <benjaminshurts@gmail.com>
2021-10-20 10:35:00 -0600

    speeder and submodule

---
+++ Assets/vfx/Scripts/VFXModel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using HoudiniEngineUnity;
+using UnityEngine;
+[System.Serializable]
+public class VFXModel
+{
+    public List<GameObject> VFXPrefabs;
+
+    public HEU_HoudiniAsset generator;
+    
+}
